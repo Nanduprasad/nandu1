dba02d315a5e84d5f6f41a1b8a5f4aec946feff6
Nanduprasad <41770107+Nanduprasad@users.noreply.github.com>
2018-07-27 15:59:52 +0530

    Update large.py

--- large.py
+++ large.py
@@ -2,8 +2,8 @@ num1=3
 num2=4
 num3=8
 if((num1>num2) and (num1>num3)):
-  print("largest is num1")
+  print num1
 elif((num2>num1) and (num2>num3)):
-  print("largest is num2")
+  print num2
 else:
-  print("largest is num3")
+  print num3
