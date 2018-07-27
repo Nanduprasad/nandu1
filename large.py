584bdb7217fbc3791a5502de7d3b35780b36213d
Nanduprasad <41770107+Nanduprasad@users.noreply.github.com>
2018-07-27 15:55:56 +0530

    Create large.py

---
+++ large.py
@@ -0,0 +1,9 @@
+num1=3
+num2=4
+num3=8
+if((num1>num2) and (num1>num3)):
+  print("largest is num1")
+elif((num2>num1) and (num2>num3)):
+  print("largest is num2")
+else:
+  print("largest is num3")
